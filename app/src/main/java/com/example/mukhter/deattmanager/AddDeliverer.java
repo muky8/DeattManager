d4bf9e6ec105ee4c7758186df46651b26883c189
Mukthar <muktharadepoju08@gmail.com>
2018-12-27 23:42:52 +0100

    first commit
    first commit

---
+++ app/src/main/java/com/example/mukhter/deattmanager/AddDeliverer.java
@@ -0,0 +1,15 @@
+package com.example.mukhter.deattmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDeliverer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_deliverer);
+
+
+    }
+}
